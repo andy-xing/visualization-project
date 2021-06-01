fdbb45d8409472b1c418e4396e52dfaeba746f40
andy.xing <991376602@qq.com>
2021-06-01 23:40:13 +0800

    添加了chart1和chart2的数据更新效果

--- visualization-project/src/components/chart-1.tsx
+++ visualization-project/src/components/chart-1.tsx
@@ -2,42 +2,69 @@ import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
 import {createEchartsOptions} from '../shared/create-echarts-options';
 
+
 export const Chart1 = () => {
   const divRef = useRef(null);
-  useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    myChart.setOption(createEchartsOptions({
-      xAxis: {
-        data: ['光明新区', '宝安区', '龙华区', '南山区', '福田区', '罗湖区', '龙岗区', '盐田区', '坪山区','大鹏区'],
-        axisTick: {show: false},
-        axisLine: {
-          lineStyle: {color: '#083B70'}
-        },
-        axisLabel: {
-          formatter(val) {
-            if (val.length > 2) {
-              const array = val.split('');
-              array.splice(2, 0, '\n');
-              return array.join('');
-            } else {
-              return val;
-            }
-          }
-        },
-      },
+  const myChart=useRef(null)
+  const data1 = [
+    {name: '光明新区',value:10},
+    {name: '宝安区', value: 20},
+    {name: '龙华区', value: 36},
+    {name: '南山区', value: 41},
+    {name: '福田区', value: 15},
 
-      yAxis: {
-        splitLine: {show: false},
-        axisLine: {
-          show: true,
-          lineStyle: {color: '#083B70'}
+  ];
+  const data2=[
+    {name: '罗湖区', value: 26},
+    {name: '龙岗区', value: 37},
+    {name: '盐田区', value: 18},
+    {name: '坪山区', value: 29},
+    {name: '大鹏区', value: 30},
+  ];
+  const data = data1;
+  useEffect(()=>{
+    setInterval(()=>{
+     x(data2)
+    },3000)
+  },[])
+const x=(data)=>{
+  myChart.current.setOption(createEchartsOptions({
+    xAxis: {
+      data: data.map(i=>i.name),
+      axisTick: {show: false},
+      axisLine: {
+        lineStyle: {color: '#083B70'}
+      },
+      axisLabel: {
+        formatter(val) {
+          if (val.length > 2) {
+            const array = val.split('');
+            array.splice(2, 0, '\n');
+            return array.join('');
+          } else {
+            return val;
+          }
         }
       },
-      series: [{
-        type: 'bar',
-        data: [10, 20, 36, 41, 15, 26, 37, 18, 29,30]
-      }]
-    }));
+    },
+
+    yAxis: {
+      splitLine: {show: false},
+      axisLine: {
+        show: true,
+        lineStyle: {color: '#083B70'}
+      }
+    },
+    series: [{
+      type: 'bar',
+      data: data.map(i=>i.value),
+    }]
+  }));
+}
+  useEffect(() => {
+    myChart.current = echarts.init(divRef.current);
+    x(data)
+
   }, []);
 
   return (
--- visualization-project/src/components/chart-2.tsx
+++ visualization-project/src/components/chart-2.tsx
@@ -4,9 +4,38 @@ import {createEchartsOptions} from '../shared/create-echarts-options';
 
 export const Chart2 = () => {
   const divRef = useRef(null);
-  useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    myChart.setOption(createEchartsOptions({
+  const myChart=useRef(null)
+const data=[
+  {name:'光明新区',2020:1,2021:2},
+  {name:'宝安区',2020:2,2021:3},
+  {name:'龙华区',2020:3,2021:4},
+  {name:'南山区',2020:4,2021:5},
+  {name:'福田区',2020:5,2021:6},
+  {name:'罗湖区',2020:6,2021:7},
+  {name:'龙岗区',2020:7,2021:8},
+  {name:'盐田区',2020:8,2021:9},
+  {name:'坪山区',2020:9,2021:10},
+  {name:'大鹏区',2020:10,2021:11},
+];
+  useEffect(()=>{
+    setInterval(()=>{
+      const newData=[
+        {name:'光明新区',2020:Math.random()*10,2021:Math.random()*10},
+        {name:'宝安区',2020:Math.random()*9,2021:Math.random()*9},
+        {name:'龙华区',2020:Math.random()*8,2021:Math.random()*8},
+        {name:'南山区',2020:Math.random()*7,2021:Math.random()*7},
+        {name:'福田区',2020:Math.random()*6,2021:Math.random()*6},
+        {name:'罗湖区',2020:Math.random()*5,2021:Math.random()*5},
+        {name:'龙岗区',2020:Math.random()*4,2021:Math.random()*4},
+        {name:'盐田区',2020:Math.random()*3,2021:Math.random()*3},
+        {name:'坪山区',2020:Math.random()*2,2021:Math.random()*2},
+        {name:'大鹏区',2020:Math.random(),2021:Math.random()},
+      ]
+      x(newData)
+    },2000)
+  },[])
+  const x=(data)=>{
+    myChart.current.setOption(createEchartsOptions({
       xAxis: {
         type: 'value',
         boundaryGap: [0, 0.01],
@@ -16,7 +45,7 @@ export const Chart2 = () => {
       yAxis: {
         axisTick: {show: false},
         type: 'category',
-        data: ['光明新区', '宝安区', '龙华区', '南山区', '福田区', '罗湖区', '龙岗区', '盐田区', '坪山区','大鹏区'],
+        data: data.map(i=>i.name),
         axisLabel: {
           formatter(val) {
             return val.replace('区', '\n区');
@@ -27,7 +56,7 @@ export const Chart2 = () => {
         {
           name: '2011年',
           type: 'bar',
-          data: [1, 2, 3, 4, 5, 6, 7, 8, 9,10],
+          data:data.map(i=>i[2020]),
           itemStyle: {
             normal: {
               color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
@@ -43,7 +72,7 @@ export const Chart2 = () => {
         {
           name: '2012年',
           type: 'bar',
-          data: [2, 3, 4, 5, 6, 7, 8, 9, 10,11],
+          data: data.map(i=>i[2021]),
           itemStyle: {
             normal: {
               color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
@@ -58,6 +87,10 @@ export const Chart2 = () => {
         }
       ]
     }));
+  }
+  useEffect(() => {
+    myChart.current = echarts.init(divRef.current);
+    x(data)
   }, []);
 
   return (
